252dd0f50872eedf7f86f2f9c5c40c0f3f9fdae3
Juanpav78 <87917164+Juanpav78@users.noreply.github.com>
2022-02-26 17:10:17 -0500

    Estructura Inicial

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+import pygame
 
+#VARIABLES GLOBALES#
+#Caracteristicas#
+fondo=pygame.image.load('Imagenes/Escenario.png')
+icono=pygame.image.load('Imagenes/Dino1idle.png')
+#Colores#
+BlueSky= (9, 187, 214)
+Orange= (242,172,81)
+BlueNigth= (25, 37, 56)
+#Ancho y alto de Pantalla#
+w=1200
+h=800
+#Inicializar Variables#
+
+#Auxiliares#
+
+#Función Recargar Pantalla#
+
+pygame.init() #Iniciar Pygame#
+Pantalla = pygame.display.set_mode((w,h))
+
+pygame.display.set_icon(icono)
+ejecutando = True #Bucle del juego#
+while ejecutando:
+  for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            ejecutando = False
