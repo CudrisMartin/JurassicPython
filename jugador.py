53a56c04a4be80f795115c874b934f5ada6f5516
Juanpav78 <87917164+Juanpav78@users.noreply.github.com>
2022-02-26 17:12:03 -0500

    Update jugador.py

--- jugador.py
+++ jugador.py
@@ -1,1 +1,17 @@
 
+Corriendo = [pygame.image.load('Imagenes/Dino1run1.png'),
+             pygame.image.load('Imagenes/Dino1idle.png'),
+             pygame.image.load('Imagenes/Dino1run2.png'),
+             pygame.image.load('Imagenes/Dino1idle.png'),
+             pygame.image.load('Imagenes/Dino1run1.png'),
+             pygame.image.load('Imagenes/Dino1idle.png')]
+
+Salto = [pygame.image.load('Imagenes/Dino1idle.png'),
+         pygame.image.load('Imagenes/Dino1idle.png'),]
+
+class Jugador(pygame.sprite.Sprite):
+    
+    def __init__(self):
+        super().__init__()
+        self.image=pygame.transform.scale(Salto[0], (150, 175))
+        self.rect = self.image.get_rect() 
