372988e5c2a18a2c5581c1a5a9b66baa74bd4da9
Juanpav78 <87917164+Juanpav78@users.noreply.github.com>
2022-03-21 14:28:08 -0500

    Clase de enemigo de Piedra agregado

--- enemigos.py
+++ enemigos.py
@@ -1,1 +1,38 @@
+import pygame
 
+posicionX = 1300
+w= 1200
+velocidad=0
+
+def obtener(contador):
+    global contadorNow 
+    contadorNow= contador
+    
+        
+def reinicio(gameover):
+    global posicionX
+    if gameover==True:
+        posicionX = 1300
+
+class Piedra(pygame.sprite.Sprite):
+    
+    def __init__(self):    
+        global posicionX
+        super().__init__()
+        self.image = pygame.transform.scale(pygame.image.load('Imagenes/Piedra.png'), (95, 120))
+        self.rect = self.image.get_rect()    
+        self.rect.center = (posicionX ,490 )  
+             
+        
+        
+    def update(self):
+        
+        global posicionX, contadorNow, velocidad
+        self.rect.center = (posicionX , 490 )  
+        if posicionX <= -450:
+            posicionX=1300
+        posicionX-=contadorNow
+
+        
+        
+        
